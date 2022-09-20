51b09795bafa7b346e3d58805ac4af1238bb6cf2
KirillV9819 <kirillv9819@gmail.com>
2022-09-20 13:04:17 +0300

    Second homework is done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkUp = createMarkUp(galleryItems);
+
+galleryRef.insertAdjacentHTML("afterbegin", galleryMarkUp);
+
+function createMarkUp(galleryItems) {
+   return galleryItems.map(({ preview, original, description }) =>
+    `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" 
+    alt="${description}" />
+    </a>`
+    )
+    .join("");
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionPosition: "bottom",
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+
+
+
